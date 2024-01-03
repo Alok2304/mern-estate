1d7bc1356c55062c35764feabd0993c9a7a32437
Alok Sandip Kale <alokkale2304@gmail.com>
2024-01-03 23:17:40 +0530

    Created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,8 +15,11 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!!`);
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
