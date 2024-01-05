dd9c17ec19721dd0813cdb06690731962b1db21e
Alok Sandip Kale <alokkale2304@gmail.com>
2024-01-05 12:49:14 +0530

    Added Redux toolkit to the project to handle global state

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+	signInStart,
+	signInSuccess,
+	signInFailure,
+} from "../redux/user/userSlice.js";
+import { useSelector } from "react-redux";
 
 export default function SignIn() {
 	const [formData, setFormData] = useState({});
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const { loading , error } = useSelector(state => state.user);
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	const handleChange = (e) => {
 		setFormData({
@@ -17,7 +24,7 @@ export default function SignIn() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			setLoading(true);
+			dispatch(signInStart());
 			const res = await fetch("/api/auth/sign-in", {
 				method: "POST",
 				headers: {
@@ -27,15 +34,13 @@ export default function SignIn() {
 			});
 			const data = await res.json();
 			if (data.success === false) {
-				setError(data.message);
-				setLoading(false);
+				dispatch(signInFailure(data.message));
 				return;
 			}
-			setLoading(false);
+			dispatch(signInSuccess(data));
 			navigate("/");
 		} catch (error) {
-			setLoading(false);
-			setError(error.message);
+			dispatch(signInFailure(error.message));
 		}
 	};
 
