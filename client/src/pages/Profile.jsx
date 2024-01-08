d3085a0c4a7fec7f22ce9dadf3d8264ba64aa997
Alok Sandip Kale <alokkale2304@gmail.com>
2024-01-08 19:27:45 +0530

    Completed profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,43 @@
+import { useSelector } from "react-redux";
 
+export default function Profile() {
+	const { currentUser } = useSelector((state) => state.user);
 
-function Profile() {
-  return (
-    <div>Profile</div>
-  )
+	return (
+		<div className="p-3 max-w-lg mx-auto">
+			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+			<form className="flex flex-col gap-6">
+				<img
+					className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+					src={currentUser.avatar}
+					alt="profile"
+				/>
+				<input
+					type="text"
+					id="username"
+					placeholder="username"
+					className="border p-3 rounded-lg"
+				/>
+				<input
+					type="email"
+					id="email"
+					placeholder="email"
+					className="border p-3 rounded-lg"
+				/>
+				<input
+					type="text"
+					id="password"
+					placeholder="password"
+					className="border p-3 rounded-lg"
+				/>
+				<button className="text-white uppercase rounded-lg p-3 bg-slate-700 hover:opacity-95 disabled:opacity-80">
+					Update
+				</button>
+			</form>
+			<div className="flex justify-between mt-5">
+				<span className="text-red-700 cursor-pointer">Delete Account</span>
+				<span className="text-red-700 cursor-pointer">Sign Out</span>
+			</div>
+		</div>
+	);
 }
-
-export default Profile;
